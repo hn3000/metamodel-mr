f33965ac9673dc7e4f07bc345375d26dea119191
Harald Niesche <harald@niesche.de>
2016-07-10 17:29:23 +0200

    extracted from metamodel

---
+++ test/allTests.ts
@@ -0,0 +1,39 @@
+
+import {
+  JsonPointerTest,
+  JsonReferenceTest
+} from "./json-ptr.test";
+
+import {
+  TestAsync
+} from "@hn3000/tsunit-async";
+
+
+export function runTests() {
+  "use strict";
+  let test = new TestAsync();
+  test.addTestClass(new JsonPointerTest(), "JsonPointerTest");
+  test.addTestClass(new JsonReferenceTest(), "JsonReferenceTest");
+
+  let promise = test.runAsync();
+  promise.then((result) => {
+    //console.log(result);
+
+    if (result.errors.length) {
+      console.log('---');
+      result.errors.forEach((e)=>{
+        console.log(`Failed: ${e.testName}.${e.funcName} - ${e.message}`);
+      });
+      console.log('---');
+      console.log(`ran unit tests, ${result.passes.length} passed, ${result.errors.length} failed`);
+    } else {
+      let testnames = result.passes.map((x)=>`${x.testName}.${x.funcName}`).join('\n');
+      console.log('---');
+      console.log(testnames);
+      console.log('---');
+      console.log(`ran unit tests, all ${result.passes.length} tests passed`);
+    }
+  });
+}
+
+runTests();
