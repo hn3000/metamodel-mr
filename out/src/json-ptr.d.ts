08b2abefb91f6f78ed7ef3102daf33af00986904
Harald Niesche <harald@niesche.de>
2017-03-01 09:27:20 +0100

    updated build result

--- out/src/json-ptr.d.ts
+++ out/src/json-ptr.d.ts
@@ -1,12 +1,21 @@
 export declare class JsonPointer {
+    static paths(obj: any, pred?: (x: any) => boolean): string[];
+    static pointers(obj: any, pred?: (x: any) => boolean): JsonPointer[];
+    /**
+     * walk obj and pass all values and their paths to the walker function
+     *
+     * stops decending into sub-objects if the walker returns true
+     */
+    static walkObject(obj: any, walker: (val: any, p: JsonPointer) => boolean): void;
+    static deref(p: string, obj: any): any;
     constructor(ref: string | string[] | JsonPointer, extraUnquoted?: string);
     add(extraUnquoted: string): JsonPointer;
     readonly parent: JsonPointer;
     get(segment: number): string;
-    readonly segments: string[];
     static unquote(s: string): string;
     static quote(s: string): string;
-    static deref(o: any, k: string): any;
+    static _deref(o: any, k: string): any;
+    static _set(o: any, k: string, v: any): void;
     getValue(obj: any): any;
     setValue(obj: any, val: any, createPath?: boolean): any;
     deleteValue(obj: any): void;
--- out/src/json-ptr.js
+++ out/src/json-ptr.js
@@ -1,5 +1,16 @@
 /* /// <reference path="../typings/index.d.ts" /> */
 "use strict";
+function isSimpleObj(val) {
+    return ((val instanceof Date)
+        || (val instanceof String)
+        || (val instanceof RegExp));
+}
+function maybeArrayIndex(val) {
+    if ('-' == val)
+        return true;
+    var n = parseFloat(val);
+    return !(isNaN(n) || Math.floor(n) !== n);
+}
 var JsonPointer = (function () {
     function JsonPointer(ref, extraUnquoted) {
         if (typeof ref === 'string') {
@@ -15,6 +26,52 @@ var JsonPointer = (function () {
             this._keypath.push(extraUnquoted);
         }
     }
+    JsonPointer.paths = function (obj, pred) {
+        var result = [];
+        if (typeof pred === 'function') {
+            JsonPointer.walkObject(obj, function (v, p) { return (pred(v) && result.push(p.toString()), false); });
+        }
+        else {
+            JsonPointer.walkObject(obj, function (_, p) { return (result.push(p.toString()), false); });
+        }
+        return result;
+    };
+    JsonPointer.pointers = function (obj, pred) {
+        var result = [];
+        if (typeof pred === 'function') {
+            JsonPointer.walkObject(obj, function (v, p) { return (pred(v) && result.push(p), false); });
+        }
+        else {
+            JsonPointer.walkObject(obj, function (_, p) { return (result.push(p), false); });
+        }
+        return result;
+    };
+    /**
+     * walk obj and pass all values and their paths to the walker function
+     *
+     * stops decending into sub-objects if the walker returns true
+     */
+    JsonPointer.walkObject = function (obj, walker) {
+        var queue = [];
+        queue.push({ val: obj, path: new JsonPointer('') });
+        while (0 != queue.length) {
+            var _a = queue.shift(), val = _a.val, path = _a.path;
+            var keys = Object.keys(val);
+            for (var _i = 0, keys_1 = keys; _i < keys_1.length; _i++) {
+                var k = keys_1[_i];
+                var thisVal = val[k];
+                var thisPath = path.add(k);
+                var more = !walker(thisVal, thisPath);
+                var thisType = typeof (thisVal);
+                if ((thisType === 'object') && more && !isSimpleObj(thisVal)) {
+                    queue.push({ val: thisVal, path: thisPath });
+                }
+            }
+        }
+    };
+    JsonPointer.deref = function (p, obj) {
+        return new JsonPointer(p).getValue(obj);
+    };
     JsonPointer.prototype.add = function (extraUnquoted) {
         return new JsonPointer(this, extraUnquoted);
     };
@@ -31,13 +88,6 @@ var JsonPointer = (function () {
         var keypath = this._keypath;
         return keypath[segment + (segment < 0 ? keypath.length : 0)];
     };
-    Object.defineProperty(JsonPointer.prototype, "segments", {
-        get: function () {
-            return this._keypath.slice();
-        },
-        enumerable: true,
-        configurable: true
-    });
     JsonPointer.unquote = function (s) {
         var result = s.replace(/~1/g, '/');
         result = result.replace(/~0/g, '~');
@@ -48,7 +98,7 @@ var JsonPointer = (function () {
         result = result.replace(/\//g, '~1');
         return result;
     };
-    JsonPointer.deref = function (o, k) {
+    JsonPointer._deref = function (o, k) {
         var isDash = k == '-';
         if (isDash) {
             var isArray = Array.isArray(o);
@@ -58,30 +108,42 @@ var JsonPointer = (function () {
         }
         return o && o[k];
     };
+    JsonPointer._set = function (o, k, v) {
+        var i = k;
+        if ((k === '-') && Array.isArray(o)) {
+            i = o.length;
+        }
+        o[i] = v;
+    };
     JsonPointer.prototype.getValue = function (obj) {
-        return this._keypath.reduce(JsonPointer.deref, obj);
+        return this._keypath.reduce(JsonPointer._deref, obj);
     };
     JsonPointer.prototype.setValue = function (obj, val, createPath) {
         if (createPath === void 0) { createPath = false; }
-        var keys = this._keypath.slice();
-        var last = keys.pop();
-        var start = obj || {};
+        var keys = this._keypath;
+        if (keys.length == 0) {
+            return val;
+        }
+        var start = obj || (maybeArrayIndex(keys[0]) ? [] : {});
         var tmp = start;
-        for (var _i = 0, keys_1 = keys; _i < keys_1.length; _i++) {
-            var k = keys_1[_i];
-            if (null == tmp[k]) {
+        var i = 0;
+        for (var n = keys.length - 1; i < n; ++i) {
+            var k = keys[i];
+            var ntmp = JsonPointer._deref(tmp, k);
+            if (null == ntmp) {
                 if (createPath || null == obj) {
-                    tmp[k] = {}; // we don't know if it should be an array
+                    ntmp = maybeArrayIndex(keys[i + 1]) ? [] : {};
+                    JsonPointer._set(tmp, k, ntmp);
                 }
                 else {
                     tmp = null;
                     break;
                 }
             }
-            tmp = tmp[k];
+            tmp = ntmp;
         }
         if (null != tmp) {
-            tmp[last] = val;
+            JsonPointer._set(tmp, keys[i], val);
         }
         return start;
     };
