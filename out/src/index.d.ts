a0a67bf55d372f0240c0b94ab0c5d1512dff196a
Harald Niesche <harald@niesche.de>
2018-08-14 12:37:57 +0200

    remove build results, no longer needed for install from git
    see "Better git support" at
    https://blog.npmjs.org/post/173240511455/the-new-npm-cli-a-year-in-review-or-what-you

--- out/src/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { JsonPointer } from './json-ptr';
-export { JsonReference } from './json-ref';
-export { JsonReferenceProcessor } from './json-ref-processor';
--- out/src/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var json_ptr_1 = require("./json-ptr");
-exports.JsonPointer = json_ptr_1.JsonPointer;
-var json_ref_1 = require("./json-ref");
-exports.JsonReference = json_ref_1.JsonReference;
-var json_ref_processor_1 = require("./json-ref-processor");
-exports.JsonReferenceProcessor = json_ref_processor_1.JsonReferenceProcessor;
-//# sourceMappingURL=index.js.map
