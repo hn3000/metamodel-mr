b54ce1f6f42bd100cb00e128754963d615d1cb06
Harald Niesche <harald@niesche.de>
2018-10-22 12:31:04 +0200

    remove bogus folder

--- package/out/src/export.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-Object.defineProperty(exports, "__esModule", { value: true });
-__export(require("./api"));
-__export(require("./metaapi"));
-__export(require("./metaapiclient"));
-//# sourceMappingURL=export.js.map
--- package/src/export.ts
+++
@@ -1,6 +0,0 @@
-
-export * from './api';
-
-export * from './metaapi';
-
-export * from './metaapiclient';
