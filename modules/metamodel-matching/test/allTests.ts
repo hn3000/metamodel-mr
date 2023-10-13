00de2cef681c62f9c1d4c42d4184d8220bd2b5cf
Harald Niesche <harald@niesche.de>
2023-10-13 16:42:25 +0200

    improved unit tests

--- modules/metamodel-matching/test/allTests.ts
+++ modules/metamodel-matching/test/allTests.ts
@@ -64,5 +64,42 @@ describe('matchmakerBuilder', () => {
       best = mm.findBest(arrayModel, '', '', undefined);
       assert.strictEqual(3, best[0], `expected entry 3, got ${JSON.stringify(best)}`);
     });
+    it('matches by color', () => {
+      const schema = {
+        id: 'model',
+        type: 'object',
+        properties: {
+          text: { type: 'string' },
+          num: { type: 'number' },
+          arr: {
+            type: 'array',
+            items: { id: 'arrayItem', type: 'object', properties: {a: {type: 'string'}, b: {type: 'number'}}}
+          }
+        }
+      };
+
+      const registry = new ModelSchemaParser();
+      const fullModel = registry.addSchemaObject(schema.id, schema);
+      assert.isNotNull(fullModel);
+      const arrayModel = fullModel.asCompositeType()!.itemType('arr');
+      assert.isNotNull(arrayModel);
+      const arrayItemModel = arrayModel.asCompositeType()!.itemType(0);
+      assert.isNotNull(arrayItemModel);
+
+      const builder = matchMakerBuilder<number>([]);
+      builder.add(1, MatchQ.typeName('model'));
+      builder.add(2, MatchQ.typeName('arrayItem'));
+      builder.add(3, MatchQ.element(MatchQ.typeName('arrayItem')));
+      const mm = builder.freeze();
+
+      let best = mm.findBest(fullModel, '', '', undefined);
+      assert.strictEqual(1, best[0], `expected entry 1, got ${JSON.stringify(best)}`);
+
+      best = mm.findBest(arrayItemModel, '', '', undefined);
+      assert.strictEqual(2, best[0], `expected entry 2, got ${JSON.stringify(best)}`);
+
+      best = mm.findBest(arrayModel, '', '', undefined);
+      assert.strictEqual(3, best[0], `expected entry 3, got ${JSON.stringify(best)}`);
+    });
   })
 })
